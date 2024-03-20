bf259da474ad9e7be55a05e00c5057f23faa566d
iverly <github@iverly.net>
2024-03-20 10:45:29 +0100

    feat(terraform): add create namespaces module
    Signed-off-by: iverly <github@iverly.net>

---
+++ terraform/modules/create-namespaces/main.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_namespace_v1" "flux-system" {
+  metadata {
+    name = "flux-system"
+  }
+}
+
+resource "kubernetes_namespace_v1" "cert-manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
