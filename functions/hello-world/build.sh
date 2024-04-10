1b22718370c6b9c5174abb1a5cff78edf8effa2f
iverly <github@iverly.net>
2024-04-10 18:30:43 +0200

    feat(function): add build script for hello world
    Signed-off-by: iverly <github@iverly.net>

---
+++ functions/hello-world/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eou pipefail
+
+# Create the deploy package directory if it doesn't exist
+mkdir -p ${DEPLOY_PKG}
+
+# Copy the index.js file to the deploy package directory
+cp ${SRC_PKG}/tibuzin-main/functions/hello-world/index.js ${DEPLOY_PKG}
