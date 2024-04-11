0be658888aceb3a680c0468f32ae556fdafa2fc2
Maxtho <maxime.pizzolitto.82@gmail.com>
2024-04-11 13:40:19 +0200

    fix: image build and route

--- functions/weather/build.sh
+++ functions/weather/build.sh
@@ -4,5 +4,10 @@ set -eou pipefail
 # Create the deploy package directory if it doesn't exist
 mkdir -p ${DEPLOY_PKG}
 
+cd ${SRC_PKG}/tibuzin-main/functions/weather
+
+npm install axios
+
 # Copy the index.js file to the deploy package directory
 cp ${SRC_PKG}/tibuzin-main/functions/weather/index.js ${DEPLOY_PKG}
+cp -R ${SRC_PKG}/tibuzin-main/functions/weather/node_modules ${DEPLOY_PKG}
